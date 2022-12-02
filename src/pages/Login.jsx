dc45a65b22034d67d93640af98ca91579c0fa3cd
Felius <codyjclose95@gmail.com>
2022-12-01 19:12:26 -0600

    Init

---
+++ src/pages/Login.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Lama Chat</span>
+        <span className="title">Register</span>
+        <form action="">
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button>Sign in</button>
+        </form>
+        <p>You don't have an account? Register</p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/pages/Register.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Add from "../img/addAvatar.png";
+
+const Register = () => {
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Lama Chat</span>
+        <span className="title">Login</span>
+        <form action="">
+          <input type="text" placeholder="Display Name" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <input style={{ display: "none" }} type="file" id="file" />
+          <label htmlFor="file">
+            <img src={Add} alt="" />
+            <span>Add an avatar</span>
+          </label>
+          <button>Sign up</button>
+        </form>
+        <p>You do have an account? Login</p>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
